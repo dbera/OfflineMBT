ac35c546681e724d7d00273c724112f8d9370f70
raulmonti <raulmonti88@gmail.com>
2025-02-17 11:22:41 +0100

    Added support for components without start event. This includes support for genarting default values for types, since tasks without input (in components without start events) need to generate tokens, which need to be initialized to default values.

--- bundles/nl.asml.matala.bpmn4s/src/nl/asml/matala/bpmn4s/bpmn4s/BaseType.java
+++ bundles/nl.asml.matala.bpmn4s/src/nl/asml/matala/bpmn4s/bpmn4s/BaseType.java
@@ -1,8 +1,11 @@
 package nl.asml.matala.bpmn4s.bpmn4s;
 
+import javax.lang.model.type.UnknownTypeException;
+
+
 public class BaseType extends Bpmn4sDataType {
 	// Int, Bool, Float, String
-	BaseType (String type) {
+	public BaseType (String type) {
 		super(type);
 		String name = "";
 		if (type.equals(Bpmn4sDataType.BOOL_TYPE)) {
@@ -15,6 +18,21 @@ public class BaseType extends Bpmn4sDataType {
 			name = "Real";
 		}
 		setName(name);
+	}
+	
+	@Override
+	public String getDefaultInit() {
+		if (type.equals(Bpmn4sDataType.BOOL_TYPE)) {
+			return "true";
+		} else if (type.equals(Bpmn4sDataType.INT_TYPE)) {
+			return "0" ;
+		} else if (type.equals(Bpmn4sDataType.STRING_TYPE)) {
+			return "";
+		} else if (type.equals(Bpmn4sDataType.FLOAT_TYPE)) {
+			return "0.0";
+		}else {
+			throw new UnknownTypeException(null, null);
+		}
 	}
 }
 
@@ -35,3 +53,4 @@ class Bpmn4s_Float extends Bpmn4sDataType {
 		super(FLOAT_TYPE);
 	}
 }
+
