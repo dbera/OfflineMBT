86c7a83a6f5b61802bb27e7bfafa1f412ddb74c6
Diego Damasceno <damasceno.diego@gmail.com>
2025-04-15 14:41:57 +0200

    checking if env variable is defined

--- bundles/nl.esi.comma.project.standard.cli/dist/start-simulator.bat
+++ bundles/nl.esi.comma.project.standard.cli/dist/start-simulator.bat
@@ -1,11 +1,18 @@
 @ECHO OFF
 SETLOCAL
 
+echo Using python environment: %BPMN4S_PYTHON%
+
+IF not defined BPMN4S_PYTHON (
+  ECHO BPMN4S_PYTHON variable is NOT defined >&2
+  pause
+  exit
+)
+
 FOR /F "delims=" %%i IN ("%~dp0") DO (
   set script_drive=%%~di
   set script_path=%%~pi
 )
-
 set simulator_file=%script_drive%%script_path%\simulator\CPNServer.py
 echo Starting simulator:  %simulator_file%
 
