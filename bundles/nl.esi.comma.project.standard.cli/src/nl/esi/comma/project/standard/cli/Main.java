c9094c7c7c3fd06938eed12ac55fed907cf7bd2f
Diego Damasceno <damasceno.diego@gmail.com>
2025-07-24 18:02:09 +0200

    project.standard.cli requires atspec bundle

--- bundles/nl.esi.comma.project.standard.cli/src/nl/esi/comma/project/standard/cli/Main.java
+++ bundles/nl.esi.comma.project.standard.cli/src/nl/esi/comma/project/standard/cli/Main.java
@@ -19,6 +19,7 @@ import nl.esi.comma.inputspecification.InputSpecificationStandaloneSetup;
 import nl.esi.comma.project.standard.StandardProjectStandaloneSetup;
 import nl.esi.comma.signature.InterfaceSignatureStandaloneSetup;
 import nl.esi.comma.testspecification.TestspecificationStandaloneSetup;
+import nl.esi.comma.abstracttestspecification.AbstractTestspecificationStandaloneSetup;
 import nl.esi.comma.types.generator.CommaMain;
 
 public class Main {
@@ -26,6 +27,7 @@ public class Main {
 	public static void main(String[] args) {
 	    InterfaceSignatureStandaloneSetup.doSetup();
 	    InputSpecificationStandaloneSetup.doSetup();
+	    AbstractTestspecificationStandaloneSetup.doSetup();
 	    TestspecificationStandaloneSetup.doSetup();
 		ProductStandaloneSetup.doSetup();
 		
