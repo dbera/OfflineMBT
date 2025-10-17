2e67e935116c1ed6107261f65679d1d1cc5bd5fa
Diego Damasceno <damasceno.diego@gmail.com>
2025-10-17 13:04:24 +0200

    mapping descriptors to atspec elements

--- bundles/nl.esi.comma.abstracttestspecification/src/nl/esi/comma/abstracttestspecification/generator/to/bpmn/DatastoreDescriptor.java
+++ bundles/nl.esi.comma.abstracttestspecification/src/nl/esi/comma/abstracttestspecification/generator/to/bpmn/DatastoreDescriptor.java
@@ -12,16 +12,39 @@
  */
 package nl.esi.comma.abstracttestspecification.generator.to.bpmn;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import nl.esi.comma.abstracttestspecification.abstractTestspecification.Binding;
 
 public class DatastoreDescriptor extends ElementDescriptor {
 
 	public List<String> consumers;
 	public String producer;
 
+	public Map<String, TaskDescriptor> consumersTaskMap = new HashMap<>();
+	public TaskDescriptor producerTask;
+	
+	public Map<String,Binding> bind_consumers = new HashMap<>();
+	public Binding bind_producer;
+	
+
 	public DatastoreDescriptor(String id,String lane, String producer, List<String> consumers) {
     	super(id,lane);
         this.producer = producer;
         this.consumers = consumers;
     }
+	
+	public Map<String, TaskDescriptor> getConsumersTaskMap() {
+		return consumersTaskMap;
+	}
+	
+	public TaskDescriptor getProducerTask() {
+		return producerTask;
+	}
+	
+	public void setProducerTask(TaskDescriptor producerTask) {
+		this.producerTask = producerTask;
+	}
 }
