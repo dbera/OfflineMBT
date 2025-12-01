f662f38b0bef21db0dd9ccb92e06a9126898e03f
Yuri Blankenstein <yuri.blankenstein@tno.nl>
2025-12-01 11:52:16 +0100

    [#187] Format code

--- bundles/nl.esi.comma.types/src/nl/esi/comma/types/utilities/EcoreUtil3.java
+++ bundles/nl.esi.comma.types/src/nl/esi/comma/types/utilities/EcoreUtil3.java
@@ -232,7 +232,7 @@ public class EcoreUtil3 extends EcoreUtil2 {
 			throw new ValidationException(diagnostics);
 		}
 	}
-	
+
 	/**
 	 * @throws ValidationException
 	 * @see {@link Diagnostician#validate(EObject)}
